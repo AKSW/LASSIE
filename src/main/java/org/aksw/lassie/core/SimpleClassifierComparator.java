7428c17613b39a258828c36586c2954817ee569b
Axel Ngonga <ngonga@informatik.uni-leipzig.de>
2013-10-02 17:08:43 +0200

    added comparator for simple classifiers

---
+++ src/main/java/org/aksw/lassie/core/SimpleClassifierComparator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.aksw.lassie.core;
+
+import de.uni_leipzig.simba.selfconfig.SimpleClassifier;
+import java.util.Comparator;
+
+/**
+ *
+ * @author ngonga
+ */
+public class SimpleClassifierComparator implements Comparator<SimpleClassifier> {
+
+    @Override
+    public int compare(SimpleClassifier o1, SimpleClassifier o2) {
+        if (o1.fMeasure > o2.fMeasure) {
+            return 1;
+        }
+        if (o1.fMeasure < o2.fMeasure) {
+            return -1;
+        }
+        return 0;
+    }
+}
