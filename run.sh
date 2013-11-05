49e850ca78f345fd9e4c32e91f169567f5a8ed10
MSherif <mohameddotsherif@gmail.com>
2013-11-05 10:35:59 +0100

    add result folder

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="org.aksw.lassie.Experiments" -Dexec.args="10 20 1 1 0.5 0.5 4 /opt/lassie/cls1ins1/" -Dexec.classpathScope=test
+mvn exec:java -Dexec.mainClass="org.aksw.lassie.Experiments" -Dexec.args="10 20 1 1 0.5 0.5 1 10 /opt/lassie/cls1ins1/" -Dexec.classpathScope=test
